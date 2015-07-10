7195744ed69146addbe8975bd031696654a39552
Chris Price <price.c@gmail.com>
2015-07-10 14:50:13 +0100

    Gridlines

--- site/src/examples/basecoin/index.js
+++ site/src/examples/basecoin/index.js
@@ -4,6 +4,93 @@
     // SVG viewbox constants
     var WIDTH = 1024, HEIGHT = 576;
 
+    // Obviously you should use ES6 modules and mutiple files for this. I'm
+    // trying to keep the example as simple (and copy/paste-able) as possible.
+    var basecoin = {};
+
+    basecoin.gridlines = function() {
+
+        return function(selection) {
+
+            selection.each(function(data) {
+
+                // Use the simplest scale we can get away with
+                var xScale = d3.scale.linear()
+                    // Define an arbitrary domain
+                    .domain([0, 1])
+                    // Use the full width
+                    .range([0, WIDTH]);
+
+                // Use the simplest scale we can get away with
+                var yScale = d3.scale.linear()
+                    // Define an arbitrary domain
+                    .domain([0, 1])
+                    // Use the full height
+                    .range([HEIGHT, 0]);
+
+                var gridline = fc.annotation.gridline()
+                    .xScale(xScale)
+                    .yScale(yScale)
+                    .xTicks(40)
+                    .yTicks(20);
+
+                d3.select(this)
+                    .call(gridline);
+            });
+        };
+    };
+
+    basecoin.series = function() {
+
+        return function(selection) {
+
+            selection.each(function(data) {
+
+                var xScale = d3.time.scale()
+                    .domain([data[0].date, data[data.length - 1].date])
+                    // Modify the range so that the series only takes up left half the width
+                    .range([0, WIDTH * 0.5]);
+
+                var yScale = d3.scale.linear()
+                    .domain(fc.util.extent(data, ['low', 'high']))
+                    // Modify the range so that the series only takes up middle third of the the width
+                    .range([HEIGHT * 0.66, HEIGHT * 0.33]);
+
+                var candlestick = fc.series.candlestick();
+
+                fc.indicator.algorithm.bollingerBands()
+                    // Modify the window size so that we more closely track the data
+                    .windowSize(8)
+                    // Modify the multiplier to narrow the gap between the bands
+                    .multiplier(1)(data);
+
+                fc.indicator.algorithm.exponentialMovingAverage()
+                    // Use a different window size so that the indicators occasionally touch
+                    .windowSize(3)(data);
+
+                var bollingerBands = fc.indicator.renderer.bollingerBands();
+
+                var ema = fc.series.line()
+                    // Reference the value computed by the EMA algorithm
+                    .yValue(function(d) { return d.exponentialMovingAverage; });
+
+                var multi = fc.series.multi()
+                    .xScale(xScale)
+                    .yScale(yScale)
+                    .series([candlestick, bollingerBands, ema])
+                    .decorate(function(g) {
+                        g.enter()
+                            .attr('class', function(d, i) {
+                                return ['candlestick', 'bollinger-bands', 'ema'][i];
+                            });
+                    });
+
+                d3.select(this)
+                    .call(multi);
+            });
+        };
+    };
+
     var dataGenerator = fc.data.random.financial()
         .mu(0.2)                     // % drift
         .sigma(0.05)                 // % volatility
@@ -12,47 +99,15 @@
 
     var data = dataGenerator(150);
 
-    var xScale = d3.time.scale()
-        .domain([data[0].date, data[data.length - 1].date])
-        // Modify the range so that the series only takes up left half the width
-        .range([0, WIDTH * 0.5]);
+    var gridlines = basecoin.gridlines();
 
-    var yScale = d3.scale.linear()
-        .domain(fc.util.extent(data, ['low', 'high']))
-        // Modify the range so that the series only takes up middle third of the the width
-        .range([HEIGHT * 0.66, HEIGHT * 0.33]);
+    d3.select('#gridlines')
+        .call(gridlines);
 
-    var candlestick = fc.series.candlestick();
-
-    fc.indicator.algorithm.bollingerBands()
-        // Modify the window size so that we more closely track the data
-        .windowSize(8)
-        // Modify the multiplier to narrow the gap between the bands
-        .multiplier(1)(data);
-
-    fc.indicator.algorithm.exponentialMovingAverage()
-        // Use a different window size so that the indicators occasionally touch
-        .windowSize(3)(data);
-
-    var bollingerBands = fc.indicator.renderer.bollingerBands();
-
-    var ema = fc.series.line()
-        // Reference the value computed by the EMA algorithm
-        .yValue(function(d) { return d.exponentialMovingAverage; });
-
-    var multi = fc.series.multi()
-        .xScale(xScale)
-        .yScale(yScale)
-        .series([candlestick, bollingerBands, ema])
-        .decorate(function(g) {
-            g.enter()
-                .attr('class', function(d, i) {
-                    return ['candlestick', 'bollinger-bands', 'ema'][i];
-                });
-        });
+    var series = basecoin.series();
 
     d3.select('#series')
         .datum(data)
-        .call(multi);
+        .call(series);
 
 })(d3, fc);
